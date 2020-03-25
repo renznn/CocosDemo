98c8d98654f2be06efd9c7f445377f0cc186d00a
cc_model <1223648944@qq.com>
2020-03-25 11:29:19 +0800

    no message

---
+++ assets/Script/loading/Loading.ts
@@ -0,0 +1,41 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+import BaseComponent from "../common/BaseComponent";
+
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class Loading extends BaseComponent {
+
+    // LIFE-CYCLE CALLBACKS:
+
+    @property(cc.ProgressBar)
+    processBarItem: cc.ProgressBar = null;
+
+    speed: 1;
+
+    onLoad() {
+        this.processBarItem.progress = 0
+    }
+
+    start() {
+
+    }
+
+    update(dt) {
+        this._updateProgressBar(dt)
+    }
+
+    _updateProgressBar(dt) {
+        if (this.processBarItem.progress < 1.0) {
+            this.processBarItem.progress += Math.random() * 0.2
+        }else{
+
+        }
+    }
+}
---
+++ assets/Script/login/Login.ts
@@ -0,0 +1,72 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+import BaseComponent from "../common/BaseComponent";
+
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class Login extends BaseComponent {
+
+    // LIFE-CYCLE CALLBACKS:
+    @property(cc.Sprite)
+    login_panel: cc.Sprite = null
+
+    onLoad() {
+        this.loadResource()
+        // this.registerAllCustomEvents([["name",this._onJsonBtnClickEvent]])
+        // this.unregisterAllCustomEvents()
+    }
+
+    start() {
+
+    }
+
+    _initWidget() {
+        // cc.log("---init widget")
+    }
+
+    _onJsonBtnClickEvent(sender) {
+        if (sender && sender.node) {
+            let name = sender.node.name
+            // cc.log(name)
+            switch (name) {
+                case "LoginPanelBtnClose":
+                    this._isShowLoginPanel(false)
+                    break;
+                case "ButtonAccountNameClear":
+                    break;
+                case "ButtonPwdClear":
+                    break;
+                case "LoginBtnReset":
+                    break;
+                case "LoginBtnLogin":
+                    break;
+                case "LoginBtnQuickStart":
+                    this._isShowLoginPanel(true)
+                    break;
+                case "LoginBtnOther":
+                    break;
+            }
+        }
+
+    }
+
+    _isShowLoginPanel(isShow: boolean) {
+        if (isShow) {
+            this.login_panel.node.active = true
+            this.login_panel.node.opacity = 0
+            this.login_panel.node.runAction(cc.fadeIn(0.5))
+        } else {
+            this.login_panel.node.runAction(cc.fadeOut(0.5))
+            // this.login_panel.node.active = false
+        }
+    }
+
+
+    update(dt) { }
+}
